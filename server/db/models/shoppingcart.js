5ff9f61d45a62297077777e4cc0df6bdfe3ecc63
Matthew Kim <matthewkim93@gmail.com>
2016-01-12 12:00:52 -0500

    Completed product model and shopping cart model

--- server/db/models/shoppingcart.js
+++ server/db/models/shoppingcart.js
@@ -5,6 +5,10 @@ var mongoose = require('mongoose');
 var cartSchema = new mongoose.Schema({
     items: [{
         quantity: {
+            type: Number,
+            default: 1
+        },
+        price: {
             type: Number
         },
         type: mongoose.Schema.Types.ObjectId,
@@ -14,9 +18,32 @@ var cartSchema = new mongoose.Schema({
 
 cartSchema.methods.addToCart = function(item) {
     this.items.push(item);
+    this.save();
 };
-// complete methods for adding to cart
-// (.addToSet method??? look it up)
+
+cartSchema.methods.updateQuantity = function(item, newQuantity) {
+    this.items[item].quantity = newQuantity;
+    this.save();
+};
+
+cartSchema.methods.removeItem = function(item) {
+    var idx = this.items.indexOf(item);
+    this.items.splice(idx, 1);
+    this.save();
+};
+
+cartSchema.methods.checkoutCart = function() {
+
+};
+
+cartSchema.virtual('totalPrice').get(function() {
+    var total = 0;
+    this.items.forEach(function(item) {
+        total += (item.price * item.quantity);
+    });
+    return total;
+});
+
 // need methods for updating inventory upon adding items
 
 mongoose.model('ShoppingCart', cartSchema);
