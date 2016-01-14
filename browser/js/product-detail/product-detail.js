67078e7d470f3733d4be77f20169424103562b3a
opperator <julie.e.oppermann@gmail.com>
2016-01-14 17:22:12 -0500

    added a back button to go back to results from product detail

--- browser/js/product-detail/product-detail.js
+++ browser/js/product-detail/product-detail.js
@@ -2,13 +2,15 @@ app.config(function($stateProvider) {
     $stateProvider.state('productDetail', {
         url: '/productDetail/:id',
         templateUrl: 'js/product-detail/product-detail.html',
-        controller: function($scope, product) {
+        controller: function($scope, product, $window) {
             $scope.product = product;
             $scope.order = {};
+            $scope.goBack = function(){
+                $window.history.back();
+            }
         },
         resolve : {
             product: function($stateParams, ProductsFactory) {
-                console.log($stateParams.id)
                 return ProductsFactory.fetchProductById($stateParams.id);
             }
 
