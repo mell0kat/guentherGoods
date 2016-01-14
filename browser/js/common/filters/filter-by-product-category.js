0efa660a4762d6a22437b1c9a5a6996ba7b46eed
Katherine Mello <kam422@cornell.edu>
2016-01-14 18:56:32 -0500

    connect ro products by category state

--- browser/js/common/filters/filter-by-product-category.js
+++ browser/js/common/filters/filter-by-product-category.js
@@ -1,7 +1,8 @@
 app.filter('filterByCategory', function() {
     return function(input, category) {
+        console.log(category, "category in filter")
         if (!category) return input;
-        
+
         var prodinCat= input.filter(function(product) {
             if (product.category.join("") === category){
                 return product;
