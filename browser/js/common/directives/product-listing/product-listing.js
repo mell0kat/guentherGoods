c26f428384375ea656dc1b34a72d9967b24ca21b
Joe Alves <joe@fullstackacademy.com>
2016-01-15 09:47:00 -0500

    Wipe commit

--- browser/js/common/directives/product-listing/product-listing.js
+++
@@ -1,6 +0,0 @@
-app.directive('productlisting', function ($state, ProductsFactory) {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/common/directives/product-listing/product-listing.html',
-    };
-});
