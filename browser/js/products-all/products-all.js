b78367f71be559ef67bc4865c34a92488735382a
Katherine Mello <kam422@cornell.edu>
2016-01-16 13:35:07 -0500

    Fixed navbar category filter

--- browser/js/products-all/products-all.js
+++ browser/js/products-all/products-all.js
@@ -17,13 +17,20 @@ app.config(function($stateProvider) {
     $stateProvider.state('productsByCategory', {
         url:'/products/:category',
         templateUrl: 'js/products-all/products-all.html',
-        controller: function($scope, products, $stateParams) {
+        controller: function($scope, products, category) {
+            console.log(category, "cat ob")
             $scope.products = products;
-            $scope.category = $stateParams.category;
+
+            $scope.category = category;
+            console.log($scope.category, "scope category")
         },
         resolve: {
             products: function($http, ProductsFactory) {
                 return ProductsFactory.fetchAll();
+            },
+            category: function($http, ProductsFactory, $stateParams) {
+                console.log($stateParams.category, "state params")
+                return ProductsFactory.fetchACategory($stateParams.category);
             }
         }
     });
