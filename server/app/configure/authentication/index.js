f2b11cc920a06bfdde8926243e9837ac059c1d60
Matthew Kim <matthewkim93@gmail.com>
2016-01-16 16:56:16 -0500

    added log for serialized user

--- server/app/configure/authentication/index.js
+++ server/app/configure/authentication/index.js
@@ -33,6 +33,7 @@ module.exports = function (app) {
 
     // When we give a cookie to the browser, it is just the userId (encrypted with our secret).
     passport.serializeUser(function (user, done) {
+        console.log('[app/config/auth]', 'returning serialized user', user);
         done(null, user.id);
     });
 
