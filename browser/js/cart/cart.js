5fbd4b6303076e9949ad8b6a507b47cb33ab954a
Nick <npalenchar@gmail.com>
2016-01-16 18:03:23 -0500

    commit

--- browser/js/cart/cart.js
+++ browser/js/cart/cart.js
@@ -1,6 +1,10 @@
 app.config(function($stateProvider){
     $stateProvider.state('cart', {
         url: '/cart',
-        templateUrl: 'js/cart/cart.html'
+        templateUrl: 'js/cart/cart.html',
+        controller: function($scope, AuthService){
+            AuthService.getLoggedInUser()
+                .then(user => console.log("[browser/js/cart.js] user: ", user));
+        }
     });
 });
