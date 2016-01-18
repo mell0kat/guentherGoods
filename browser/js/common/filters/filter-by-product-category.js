43d63ce9d209bb5b8fa330f11c3ce3acbdff01a6
Katherine Mello <kam422@cornell.edu>
2016-01-18 15:36:38 -0500

    data validations

--- browser/js/common/filters/filter-by-product-category.js
+++ browser/js/common/filters/filter-by-product-category.js
@@ -1,6 +1,7 @@
 app.filter('filterByCategory', function() {
     //input needs to be category._id
     return function(input, category) {
+        if (!category) return input;
         if (!category.length) return input;
         category = category[0];
         var prodinCat= input.filter(function(product) {
