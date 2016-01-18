98434b487361fc9795933edc55425c797caafe09
opperator <julie.e.oppermann@gmail.com>
2016-01-18 14:10:08 -0500

    updated jumbotron for home page and added some navbar font styling

--- browser/js/common/factories/FullstackPics.js
+++ browser/js/common/factories/FullstackPics.js
@@ -1,8 +1,13 @@
+
 app.factory('jumbotronPics', function () {
     return [
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/products/Shuffle/shuffletable_zpsskoxw4ti.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/products/Shuffle/shufflecatbed_zpsjr8j2xzx.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/products/Shuffle/shuffletshirt_zpsbvaspsuu.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/products/Shuffle/shuffledining_zpsdfslsyiu.jpg'
+      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/cat-ladyshuffle_zpshraez3ah.jpg',
+      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/coathanger-jumbotron_zpsg3vzlfbw.jpg',
+      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/shower-jumbotron_zps04ymgl8b.jpg',
+      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/pillows-jumbotron_zps0m3ylb6r.jpg',
+      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/tablecloth-jumbotron_zpshdpdunjm.jpg',
+      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/bedsheets-jumbotron_zpslbbkz2kz.jpg'
+      
     ];
 });
+
