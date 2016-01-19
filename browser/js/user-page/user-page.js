bd830b5f82762f51f67769d94df3336b1ae8c635
Matthew Kim <matthewkim93@gmail.com>
2016-01-19 10:45:12 -0500

    working on user page

--- browser/js/user-page/user-page.js
+++ browser/js/user-page/user-page.js
@@ -2,9 +2,8 @@ app.config(function($stateProvider){
     $stateProvider.state('userProfile', {
         url: '/profile',
         templateUrl: 'js/user-page/user-page.html',
-        controller: function($scope, AuthService, $state){
-            AuthService.getLoggedInUser()
-                .then(user => { $scope.user = user });
+        controller: function($scope, AuthService, $state, user){
+            $scope.user = user;
             $scope.goToUsers = function () {
                 if ($scope.user.isAdmin) $state.go('userList');
                 else $state.go('/');
@@ -17,7 +16,15 @@ app.config(function($stateProvider){
                 if ($scope.user.isAdmin) $state.go('userAdd');
                 else $state.go('/');
             };
-
+            $scope.goToAddUsers = function() {
+                if ($scope.user.isAdmin) $state.go('productCreate');
+                else $state.go('/');
+            };
+        },
+        resolve: {
+            user: function (AuthService) {
+                return AuthService.getLoggedInUser();
+            }
         }
     });
 });
