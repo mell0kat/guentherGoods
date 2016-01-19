0701ad327b3147322ae089f866fd9c92401d40ec
Matthew Kim <matthewkim93@gmail.com>
2016-01-19 11:50:47 -0500

    working on editing individual products page

---
+++ browser/js/product-edit/product-edit.js
@@ -0,0 +1,23 @@
+app.config(function($stateProvider) {
+    $stateProvider.state('productEdit', {
+        url: '/product/edit/:id',
+        templateUrl: 'js/product-edit/product-edit.html',
+        controller: function($scope, product, user) {
+            $scope.product = product;
+            $scope.user = user;
+
+        },
+        resolve : {
+            product: function($stateParams, ProductsFactory) {
+                return ProductsFactory.fetchProductById($stateParams.id);
+            },
+            user: function(AuthService, UserFactory){
+                return AuthService.getLoggedInUser()
+                    .then(userData => {
+                        if(!userData) return UserFactory.fetchGuestUser();
+                        else return userData;
+                    })
+            }
+        }
+    });
+});
