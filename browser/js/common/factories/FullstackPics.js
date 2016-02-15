8ad5eb0f553676de3d88193379b7d4151e252e4b
opperator <julie.e.oppermann@gmail.com>
2016-02-15 12:07:23 -0500

    replaced page image links because images moved to imgur

--- browser/js/common/factories/FullstackPics.js
+++ browser/js/common/factories/FullstackPics.js
@@ -1,15 +1,15 @@
 
 app.factory('jumbotronPics', function () {
     return [
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/cat-lady-jumbotron_zpsbd6td1pg.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/coathanger-jumbotron_zpsg3vzlfbw.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/shower-jumbotron_zps04ymgl8b.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/toilet-jumbotron_zps1zeglfp3.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/socks-jumbotron_zpsgehpkm1d.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/pillows-jumbotron_zps0m3ylb6r.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/tablecloth-jumbotron_zpshdpdunjm.jpg',
-      'http://i1382.photobucket.com/albums/ah279/npalenchar/jumbotron/bedsheets-jumbotron_zpslbbkz2kz.jpg'
-      
+      'http://i.imgur.com/14rxGWj.jpg',
+      'http://i.imgur.com/z08QUuj.jpg',
+      'http://i.imgur.com/dpNsoLI.jpg',
+      'http://i.imgur.com/6fN4PSW.jpg',
+      'http://i.imgur.com/8tKbrxu.jpg',
+      'http://i.imgur.com/SQ07YQj.jpg',
+      'http://i.imgur.com/GX5Jmdi.jpg',
+      'http://i.imgur.com/UdAPRBC.jpg'
+
     ];
 });
 
